1520a789fd8b3c0c2033a20beb51154372e39210
Samuel Sulo <samuelsulo0802@gmail.com>
2021-07-22 20:18:24 +0200

    Update Footer.js

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -16,7 +16,7 @@ export default function StickyFooter() {
             <div className={classes.footerSocialLinks}>
               <a href="https://www.linkedin.com/in/samuel-sulo-669298209/" className={classes.link}><LinkedInIcon /></a>
               <a href="https://www.instagram.com/samuelsulo/" className={classes.link}><InstagramIcon /></a>
-              <a href="https://github.com/Samuel-Sulo/astro-social-media" className={classes.link}><GitHubIcon /></a>
+              <a href="https://github.com/Samuel-Sulo/astro" className={classes.link}><GitHubIcon /></a>
             </div>
             <div className={classes.footerCopyright}>
               <Copyright />
